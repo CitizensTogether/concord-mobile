b5995a803f33b204e4ee29b52dc168267d350a58
Eric Boysen <50427509+eboysen@users.noreply.github.com>
2020-08-05 15:43:28 -0500

    chore: Create sign-in screen (#7)
    * feature/common-widgets: created most frequent widgets in app
    
    * style: Apply code style suggestions from code review
    
    Adds spaces and remove comments where necessary.
    
    * Update lib/main.dart
    
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>
    
    * Update lib/ui/widgets/tags.dart
    
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>
    
    * Update lib/ui/widgets/layout_widgets.dart
    
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>
    
    * Update lib/ui/widgets/tags.dart
    
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>
    
    * Update lib/ui/widgets/tags.dart
    
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>
    
    * Update lib/ui/widgets/tags.dart
    
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>
    
    * feature/common-widgets: updated docs
    
    * feature/common-widgets: updated gitignore
    
    * feature/common-widgets: added doc comment for request details screen
    
    * feature/common-widgets: updated readme
    
    * feature/common-widgets: fixed comments for community bulletin tile
    
    * feat: Create auth screen
    
    * style: improved code style for improved readability
    
    * style: futher code style improvements
    
    * style: Updated theme/color usage and code style
    
    * fix: Keyboard dismissal bug
    
    * chore: Update documentation and refactor widget structure
    
    Also update pubspec.lock
    
    * chore: remove googleFonts usage
    
    * style: Fix spacing issues
    
    * chore: update to dynamic theme locator
    
    Co-authored-by: Neha Rode <neha9501@gmail.com>
    Co-authored-by: Willie Chalmers III <willie@citizenstogether.us>

--- lib/ui/widgets/tags.dart
+++ lib/ui/widgets/tags.dart
@@ -3,18 +3,17 @@ import 'package:flutter_tags/flutter_tags.dart';
 import '../../utils/locator.dart';
 import '../theme/theme.dart';
 
-
-/// A tags list that allows the user to be able to add and remove tags by default.
-/// 
-/// This widget is highly customizable and can also be used for static lists of tags by
-/// providing a List<String> to the [initTags] argument and marking bools [textFieldEnabled] 
-/// and [removable] as false. 
+/// A list that allows the user to be able to add and remove bulletin tags.
+///
+/// This widget is highly customizable and can also be used for static lists of
+/// tags by providing a [List] to the [initTags] argument and marking
+/// [textFieldEnabled] and [removable] as false.
 class TagPicker extends StatefulWidget {
-
-  /// Permits user to add chpis to a list by searching in a text field.
+  /// Permits user to add chips to a list by searching in a text field.
   final bool textFieldEnabled;
 
-  /// Permits user to remove tags from a list by pressing a remove button in the chip.
+  /// Permits user to remove tags from a list by pressing a remove button in the
+  /// chip.
   final bool removable;
 
   /// The background color of the chips.
@@ -23,64 +22,64 @@ class TagPicker extends StatefulWidget {
   /// The border color of the chips.
   final Color borderColor;
 
-  /// The color of the remove button (only viewable if [removable] is true).
+  /// The color of the remove button (viewable only if [removable] is true).
   final Color removeButtonColor;
 
   /// The color of the label of a chip.
   final Color textColor;
 
-  /// A list of chips to display before the user adds or removes chips from a list.
+  /// A list of chips to display before the user adds or removes chips from a
+  /// list.
   final List<String> initTags;
 
-  /// A list of possible text suggestions that are offered as autocomplete when the user types
-  /// in the text field (only viewable if [textFieldEnabled] is true).
+  /// A list of possible text suggestions that are offered as autocomplete when
+  /// the user types in the text field (viewable only if [textFieldEnabled] is
+  /// true).
   final List<String> suggestions;
 
   /// Permits the list of chips to be horizontally scrollable.
   final bool horizontalScrollEnabled;
 
-  /// The placeholder text in the text field (only viewable if [textFieldEnabled] is true).
+  /// The placeholder text in the text field (viewable only if
+  /// [textFieldEnabled] is true).
   final String hintText;
 
-  /// The list of tags the user sees. 
-  List<String> tags;
+  /// The list of tags the user sees.
+  final List<String> tags;
 
   TagPicker(
-    {Key key,
-    this.initTags,
-    this.suggestions,
-    this.textFieldEnabled = true,
-    this.removable = true,
-    this.chipColor,
-    this.borderColor,
-    this.textColor,
-    this.removeButtonColor,
-    this.hintText = "Add a skill or certification",
-    this.horizontalScrollEnabled = false
-  }) : super(key: key);
+      {Key key,
+      this.initTags,
+      this.suggestions,
+      this.textFieldEnabled = true,
+      this.removable = true,
+      this.chipColor,
+      this.borderColor,
+      this.textColor,
+      this.removeButtonColor,
+      this.hintText = 'Add a skill or certification',
+      this.horizontalScrollEnabled = false,
+      this.tags})
+      : super(key: key);
 
-  
   @override
   _TagPickerState createState() => _TagPickerState();
 }
 
 /// Keeps track of the state of TagPicker, in which [List<String> tags] is a state-having object.
 class _TagPickerState extends State<TagPicker> {
-
   final ConcordThemeData _appTheme = locator<ConcordThemeManager>().theme;
 
   @override
   void initState() {
     super.initState();
-    print('init');
-    widget.tags = widget.initTags ?? [];
+    widget.tags.addAll(widget.initTags ?? []);
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Tags(
-      key: Key("tags"),
+      key: Key('tags'),
       symmetry: false,
       columns: 0,
       horizontalScroll: widget.horizontalScrollEnabled,
@@ -96,8 +95,8 @@ class _TagPickerState extends State<TagPicker> {
           setState(() {
             widget.tags.add(string);
           });
-        }
-      ) : null,
+        },
+      ),
       itemCount: widget.tags != null ? widget.tags.length : 0,
       itemBuilder: (index) {
         final String item = widget.tags[index];
@@ -109,23 +108,25 @@ class _TagPickerState extends State<TagPicker> {
           pressEnabled: false,
           border: Border.all(
             width: 1,
-            color: widget.borderColor ?? _appTheme.mainColor
+            color: widget.borderColor ?? _appTheme.secondaryText,
           ),
           elevation: 0.0,
           activeColor: widget.chipColor ?? _appTheme.backgroundColor.withOpacity(0.5),
           borderRadius: BorderRadius.circular(30),
           combine: ItemTagsCombine.onlyText,
-          textActiveColor: widget.textColor ?? _appTheme.mainText,
-          removeButton: widget.removable ?
-            ItemTagsRemoveButton(
-              backgroundColor: widget.removeButtonColor ?? _appTheme.secondaryText,
-              onRemoved: () {
-                setState(() {
-                  widget.tags.removeAt(index);
-                });
-                return true;
-              }
-            ) : null,
+          textActiveColor: widget.textColor ?? _appTheme.secondaryText,
+          removeButton: widget.removable
+              ? ItemTagsRemoveButton(
+                  backgroundColor:
+                      widget.removeButtonColor ?? _appTheme.secondaryText,
+                  onRemoved: () {
+                    setState(() {
+                      widget.tags.removeAt(index);
+                    });
+                    return true;
+                  },
+                )
+              : null,
         );
       },
     );
